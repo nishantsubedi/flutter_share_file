131d66f11797f65f2c62221dfa1e99754c935c7b
Nishant <nishantsubedi1996@gmail.com>
2019-05-17 16:12:50 +0545

    Share Link Attached with Image: Android Only

--- android/src/main/java/com/example/fluttersharefile/FlutterShareFilePlugin.java
+++ android/src/main/java/com/example/fluttersharefile/FlutterShareFilePlugin.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import java.io.File;
+import java.util.HashMap;
 
 import io.flutter.app.FlutterActivity;
 
@@ -30,18 +31,23 @@ public class FlutterShareFilePlugin extends FlutterActivity implements MethodCal
   @Override
   public void onMethodCall(MethodCall call, Result result) {
     if (call.method.equals("shareimage")) {
-      shareFile((String) call.arguments);
+      Object arguments = call.arguments;
+      HashMap<String, String> argsMap = (HashMap<String, String>) arguments;
+      String fileName = argsMap.get("fileName");
+      String message = argsMap.get("message");
+      shareFile(fileName, message);
     } else {
       result.notImplemented();
     }
   }
 
-  private void shareFile(String path) {
-    File imageFile = new File(instance.activeContext().getCacheDir(), path);
+  private void shareFile(String fileName, String message) {
+    File imageFile = new File(instance.activeContext().getCacheDir(), fileName);
     Uri contentUri = FileProvider.getUriForFile(instance.activeContext(), "com.example.fluttersharefile", imageFile);
     Intent shareIntent = new Intent(Intent.ACTION_SEND);
     shareIntent.setType("image/png");
     shareIntent.putExtra(Intent.EXTRA_STREAM, contentUri);
+    shareIntent.putExtra(Intent.EXTRA_TEXT, message);
     instance.activity().startActivity(Intent.createChooser(shareIntent, "Share image using"));
 }
 }
--- lib/flutter_share_file.dart
+++ lib/flutter_share_file.dart
@@ -6,13 +6,17 @@ class FlutterShareFile {
   static const MethodChannel _channel =
       const MethodChannel('flutter_share_file');
 
-  static Future<bool> shareImage(String path, String fileName) async {
+  static Future<bool> shareImage(String path, String fileName, [String message = '']) async {
     bool result;
     if (Platform.isAndroid) {
-      result = await _channel.invokeMethod('shareimage', fileName);
+      // For Android send only the file name of image in temporary Directory
+      result = await _channel.invokeMethod(
+          'shareimage', {"fileName": fileName, "message": message});
     } else if (Platform.isIOS) {
+      // For iOS send the full path of image in temporary Directory
       final Map<String, dynamic> params = <String, dynamic>{
         'text': path + '/' + fileName,
+        'message': message,
         'type': 'image'
       };
       result = await _channel.invokeMethod('shareimage', params);
